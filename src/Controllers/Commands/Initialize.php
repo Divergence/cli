02b9c1ac9b17cc36e4f61d43aea88fc57f08d60b
Henry Paradiz <henry.paradiz@gmail.com>
2018-05-21 01:11:32 -0400

    Command line db connection configuration builder
    Detects default DB config if you run `divergence init` and optionally runs you through database config setup.

--- src/Controllers/Commands/Initialize.php
+++ src/Controllers/Commands/Initialize.php
@@ -1,8 +1,23 @@
 <?php
+/*
+ * This file is part of the Divergence package.
+ *
+ * (c) Henry Paradiz <henry.paradiz@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Divergence\CLI\Controllers\Commands;
 
+use Divergence\App;
 use Divergence\CLI\Command;
 use Divergence\CLI\Env;
+
+/*
+ *  @package Divergence\CLI
+ *  @author  Henry Paradiz <henry.paradiz@gmail.com>
+ */
 
 class Initialize {
     public static function init()
@@ -85,6 +100,37 @@ class Initialize {
         }
     }
 
+    /*
+     *  
+     *  Adds a PSR-4 namespace to composer.json
+     *  Asks if you want to run `composer install` and runs it
+     *  Runs Env::getEnvironment() once to make sure it was succesful
+     */
+    public static function installAutoloader()
+    {
+        $suggestedName = explode('/',Env::$package['name'])[1];
+        $input = $climate->confirm('Do you want to create a namespace called '.$suggestedName.' mapped to directory src?');
+        $input->defaultTo('y');
+        if($input->confirmed()) {
+            Env::$package['autoload']['psr-4'][$suggestedName."\\"] = 'src/';
+            Env::setPKG(getcwd(),Env::$package);
+            Env::getEnvironment();
+
+            $input = $climate->confirm('Run composer install to register new autoloaded folder?');
+            $input->defaultTo('y');
+            if($input->confirmed()) {
+                shell_exec('composer install --ansi');
+            }
+        }
+    }
+
+    /*
+     *  Checks autoloader config for a namespace to use.
+     *  Installs one if none found.
+     *  Asks to use if only one found.
+     *  TODO: prompt to select if more than 
+     *  Sets Env::$namespace once it's found.
+     */
     public static function initAutoloader()
     {
         $climate = Command::getClimate();
@@ -93,26 +139,12 @@ class Initialize {
 
         if(!count($autoloaders)) {
             $climate->info('No local autoloaded directory found!');
-            // prompt: do you want to create a new namespace? default: name of this package from composer.json
-            $suggestedName = explode('/',Env::$package['name'])[1];
-            $input = $climate->confirm('Do you want to create a namespace called '.$suggestedName.' mapped to directory src?');
-            $input->defaultTo('y');
-            if($input->confirmed()) {
-                Env::$package['autoload']['psr-4'][$suggestedName."\\"] = 'src/';
-                Env::setPKG(getcwd(),Env::$package);
-                Env::getEnvironment();
-                $autoloaders = Env::$autoloaders;
-
-                $input = $climate->confirm('Run composer install to register new autoloaded folder?');
-                $input->defaultTo('y');
-                if($input->confirmed()) {
-                    shell_exec('composer install --ansi');
-                }
-            }
+            static::installAutoloader();
+            $autoloaders = Env::$autoloaders;
         }
 
+        // if only one autoloader ask if we should use this to initialize
         if(count($autoloaders) === 1) {
-            // prompt: found 1 autoloader config. Is this your namespace?
             $key = array_keys($autoloaders)[0];
             $climate->info('Found a single autoloaded namespace: '.$key.' => loaded from ./'.$autoloaders[$key]);
             $input = $climate->confirm('Initialize at this namespace?');
@@ -130,6 +162,46 @@ class Initialize {
 
     public static function initDatabase()
     {
+        $climate = Command::getClimate();
+        App::init(getcwd());
+        error_reporting(E_ALL ^E_WARNING ^E_NOTICE); // fix error reporting cause App::init acts like it's in production
+        $config = App::config('db');
+        
+        $defaults = require getcwd().'/vendor/divergence/divergence/config/db.php';
+        
+        foreach($config as $label=>$dbconf) {
+            if($dbconf === $defaults[$label]) {
+                if(in_array($label,['mysql','dev-mysql'])) {
+                    $climate->info(sprintf('Detected default database config %s',$label));
+                    static::databaseConfigBuilder($label,$defaults[$label]);
+                }
+            }
+        }
+    }
 
+    public static function databaseConfigBuilder($label,$defaults)
+    {
+        $climate = Command::getClimate();
+        $input = $climate->inline('Config database config '.$label.'?');
+        $input->defaultTo('y');
+        $climate->bold('Configuring '.$label);
+        $new = [];
+        if($input->confirmed()) {
+            $suggestedName = explode('/',Env::$package['name'])[1];
+
+            $input = $climate->input(sprintf('Hostname (You can also provide a socket) [<bold>%s</bold>]',$defaults['host']));
+            $input->defaultTo($defaults['host']);
+            $new['host'] = $input->prompt();
+            $input = $climate->input(sprintf('Database name [<bold>%s</bold>]',$suggestedName));
+            $input->defaultTo($suggestedName);
+            $new['database'] = $input->prompt();
+            $input = $climate->input(sprintf('Database username [<bold>%s</bold>]',$suggestedName));
+            $input->defaultTo($suggestedName);
+            $new['username'] = $input->prompt();
+            $input = $climate->input('Database password (leave blank to auto-generate)');
+            $new['password'] = $input->prompt();
+        }
+
+        dump($new);
     }
 }
